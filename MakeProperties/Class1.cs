a9dbf6eeb9a704cf95c8ce95ebbdd6dbf869f557
Admin <duneesha.2017336@Iiit.ac.lk>
2021-06-09 09:35:44 +0530

    Add functions to make the image green and blue

--- MakeProperties/Class1.cs
+++ MakeProperties/Class1.cs
@@ -98,4 +98,55 @@ namespace MakeProperties
        
     }
 
+    public class MakeBlue : IFilter
+    {
+        public string Name => "Make Blue";
+
+
+
+        public Image RunPlugin(Image src)
+        {
+            var bitmap = new Bitmap(src);
+            for (int row = 0; row < bitmap.Height; ++row)
+            {
+                for (int col = 0; col < bitmap.Width; ++col)
+                {
+                    Color color = bitmap.GetPixel(col, row);
+                    if (color.B > 0)
+                    {
+                        color = Color.FromArgb(color.A, color.R, color.G, 255);
+                    }
+                    bitmap.SetPixel(col, row, color);
+                }
+            }
+
+            return bitmap;
+
+        }
+    }
+
+    public class MakeGreen : IFilter
+    {
+        public string Name => "Make Green";
+        public Image RunPlugin(Image src)
+        {
+            var bitmap = new Bitmap(src);
+            for (int row = 0; row < bitmap.Height; ++row)
+            {
+                for (int col = 0; col < bitmap.Width; ++col)
+                {
+                    Color color = bitmap.GetPixel(col, row);
+                    if (color.G > 0)
+                    {
+                        color = Color.FromArgb(color.A, color.R, 255, color.G);
+                    }
+                    bitmap.SetPixel(col, row, color);
+                }
+            }
+
+            return bitmap;
+
+        }
+    }
+
 }
